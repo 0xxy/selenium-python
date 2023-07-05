dae325a739ae3d9ba7263281415a84b14ce19db5
0xxy <oksenyuk01@inbox.ru>
2023-07-05 11:29:45 +0300

    change name of function

--- pytest/test_items.py
+++ pytest/test_items.py
@@ -1,7 +1,7 @@
 from selenium.webdriver.common.by import By
 import time
 
-def test_guest_should_see_login_link(browser):
+def test_find_card_button(browser):
     browser.get('http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/')
     length = len(browser.find_elements(By.CLASS_NAME, "btn-add-to-basket"))
 
